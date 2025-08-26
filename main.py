1f44f7e0ca2996aa463f2409770da1031ef007e2
AlexandreHRFilho <alexandrehrfilho@gmail.com>
2025-08-25 21:54:42 -0300

    pequena funcionalidade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     print("Hello from rascador-site!")
     print("Funcionalidade de teste adicionada!")
+    print("Brench pequena-funcionalidade")
 
 if __name__ == "__main__":
     main()
